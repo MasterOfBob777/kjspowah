40e483a9495914ce3aaf004c27bf0c5415a6b246
ianm1647 <ianmcg1128@gmail.com>
2025-01-10 17:48:11 -0600

    ported to 1.21.1, not sure if the events work

--- src/main/java/com/bobvaraioa/kubejspowah/KubeJSPowahPlugin.java
+++ src/main/java/com/bobvaraioa/kubejspowah/KubeJSPowahPlugin.java
@@ -1,70 +1,78 @@
 package com.bobvaraioa.kubejspowah;
 
-import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.event.EventGroup;
+import dev.latvian.mods.kubejs.event.EventGroupRegistry;
 import dev.latvian.mods.kubejs.event.EventHandler;
-import dev.latvian.mods.kubejs.event.EventJS;
-import dev.latvian.mods.kubejs.item.InputItem;
-import dev.latvian.mods.kubejs.item.OutputItem;
+import dev.latvian.mods.kubejs.event.KubeEvent;
+import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
 import dev.latvian.mods.kubejs.recipe.RecipeKey;
-import dev.latvian.mods.kubejs.recipe.component.ItemComponents;
+import dev.latvian.mods.kubejs.recipe.component.ComponentRole;
+import dev.latvian.mods.kubejs.recipe.component.IngredientComponent;
+import dev.latvian.mods.kubejs.recipe.component.ItemStackComponent;
 import dev.latvian.mods.kubejs.recipe.component.NumberComponent;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
-import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
+import dev.latvian.mods.kubejs.recipe.schema.RecipeSchemaRegistry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import org.checkerframework.checker.units.qual.C;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Fluid;
 import owmii.powah.api.PowahAPI;
 
-public class KubeJSPowahPlugin extends KubeJSPlugin {
+import java.util.List;
+
+public class KubeJSPowahPlugin implements KubeJSPlugin {
     public static EventGroup GROUP = EventGroup.of("PowahEvents");
     public static EventHandler COOLANTS = GROUP.server("registerCoolants", () -> CoolantsEvent.class);
     public static EventHandler HEAT_SOURCE = GROUP.server("registerHeatSource", () -> HeatSourceEvent.class);
     public static EventHandler MAGMATIC_FLUID = GROUP.server("registerMagmaticFluid", () -> MagmaticFluidEvent.class);
 
     interface EnergizingOrbRecipe {
-        RecipeKey<InputItem[]> INPUTS = ItemComponents.UNWRAPPED_INPUT_ARRAY.key("ingredients");
-        RecipeKey<OutputItem> OUTPUT = ItemComponents.OUTPUT.key("result");
-        RecipeKey<Long> ENERGY = NumberComponent.LONG.key("energy");
+        RecipeKey<List<Ingredient>> INPUTS = IngredientComponent.UNWRAPPED_INGREDIENT_LIST.key("ingredients", ComponentRole.INPUT);
+        RecipeKey<ItemStack> OUTPUT = ItemStackComponent.ITEM_STACK.key("result", ComponentRole.OUTPUT);
+        RecipeKey<Long> ENERGY = NumberComponent.LONG.key("energy", ComponentRole.OTHER);
 
         RecipeSchema SCHEMA = new RecipeSchema(INPUTS, OUTPUT, ENERGY);
     }
 
     @Override
-    public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
-        event.register(new ResourceLocation("powah:energizing"), EnergizingOrbRecipe.SCHEMA);
+    public void registerRecipeSchemas(RecipeSchemaRegistry event) {
+        event.register(ResourceLocation.fromNamespaceAndPath("powah", "energizing"), EnergizingOrbRecipe.SCHEMA);
     }
 
     @Override
-    public void registerEvents() {
-        GROUP.register();
+    public void registerEvents(EventGroupRegistry registry) {
+        registry.register(GROUP);
     }
 
-    static class CoolantsEvent extends EventJS {
+    static class CoolantsEvent implements KubeEvent {
         public static CoolantsEvent INSTANCE = new CoolantsEvent();
-        public void addFluid(ResourceLocation res, int cool) {
-            PowahAPI.registerCoolant(res, cool);
+        public void addFluid(Fluid fluid) {
+            PowahAPI.getCoolant(fluid);
         }
 
         public void addSolid(ItemStack res, int cool) {
-            PowahAPI.registerSolidCoolant(res.getItem(), res.getCount(), cool);
+            PowahAPI.getSolidCoolant(res.getItem());
         }
     }
 
-    static class HeatSourceEvent extends EventJS {
+    static class HeatSourceEvent implements KubeEvent {
         public static HeatSourceEvent INSTANCE = new HeatSourceEvent();
 
-        public void add(ResourceLocation res, int heat) {
-            PowahAPI.registerHeatSource(res, heat);
+        public void addBlock(Block block) {
+            PowahAPI.getHeatSource(block);
+        }
+
+        public void addFluid(Fluid fluid) {
+            PowahAPI.getHeatSource(fluid);
         }
     }
 
-    static class MagmaticFluidEvent extends EventJS {
+    static class MagmaticFluidEvent implements KubeEvent {
         public static MagmaticFluidEvent INSTANCE = new MagmaticFluidEvent();
 
-        public void add(ResourceLocation res, int heat) {
-            PowahAPI.registerMagmaticFluid(res, heat);
+        public void add(Fluid fluid) {
+            PowahAPI.getMagmaticFluidEnergyProduced(fluid);
         }
     }
 }
